afac23f9b2f25ea480ce950fc8d557163d199452
Teoro <Foromoernest.teoro@orange-sonatel.com>
2019-07-20 18:18:58 +0000

    initialisation du ws paiement en ligne

---
+++ src/main/kotlin/com/paiement/soneayh/SoneayhApplication.kt
@@ -0,0 +1,11 @@
+package com.paiement.soneayh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoneayhApplication
+
+fun main(args: Array<String>) {
+    runApplication<SoneayhApplication>(*args)
+}
